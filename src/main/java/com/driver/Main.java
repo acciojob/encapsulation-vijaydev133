4d8ec4f15b1580ce3902de62eb5b1c7fba2c27cc
vijaydev133 <vijaydeveloper004@gmail.com>
2024-01-30 18:43:23 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+	  RWOnly rw = new RWOnly();
+//	  rw.name
+//	  name is not visible
+	  rw.setName("vijay");
+	  System.out.println(rw.getName());
+  }
 }
